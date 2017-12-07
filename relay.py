ae188ae281527a3170a41f7b8f94036cf9a9e8f8
Unknown <brandon.aleson@gmail.com>
2017-12-07 12:32:59 -0800

    relay initialization function

--- relay.py
+++ relay.py
@@ -19,17 +19,23 @@ import time
 import gpiozero
 
 
-def initialize_pin(pin):
+def _initialize_pin(pin):
     '''active_high = False initializes Sainsmart 4-channel relay board as off'''
 
     return gpiozero.OutputDevice(pin, active_high=False)
 
 
+def initialize_relays(pins):
+    '''returns list of gpiozero.OutputDevice objects'''
+
+    return [_initialize_pin(pin) for pin in pins]
+
+
 if __name__ == '__main__':
-    pins = [4, 5, 6]  # list to store GPIO pins being used
-    relays = [initialize_pin(pin) for pin in pins]  # list to store initialized GPIO pins for controlling relays
+    pins = [4, 5, 6]  # GPIO pins being used
+    relays = intialize_relays(pins)
 
     for relay in relays:
         relay.on()
-        time.sleep(2)
+        time.sleep(1)
         relay.off()
