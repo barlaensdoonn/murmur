cb1f9296fad4bf72bc13a86f6b9c7db7a27e7a1f
Unknown <brandon.aleson@gmail.com>
2017-12-08 19:44:18 -0800

    pins not relays

--- relay.py
+++ relay.py
@@ -44,7 +44,7 @@ class Relay(gpiozero.OutputDevice):
 
 if __name__ == '__main__':
     pins = [4, 5, 6]  # list to store GPIO pins being used
-    relays = [Relay(pin) for relay in relays]  # list to hold Relay objects
+    relays = [Relay(pin) for pin in pins]  # list to hold Relay objects
 
     # loop through the relays, turning them on for 1 second
     for relay in relays:
