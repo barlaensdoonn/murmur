fafbf26d50b0f0010278a6699c3c2624c0bc6c11
Unknown <brandon.aleson@gmail.com>
2017-12-08 19:40:08 -0800

    extend gpiozero's OutputDevice class instead of making our own

--- relay.py
+++ relay.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # murmur - single relay class
 # 12/7/17
-# updated: 12/7/17
+# updated: 12/8/17
 
 '''
 raspi connections to Sainsmart 4-channel 5V relay board:
@@ -19,21 +19,27 @@ import time
 import gpiozero
 
 
-class Relay(object):
+class Relay(gpiozero.OutputDevice):
+    '''
+    class to extend gpiozero's OutputDevice base class
 
-    def __init__(self, pin):
-        self.pin = pin
-        self.relay = self._initialize_relay()
+    OutputDevice has several useful methods including on(), off(), and toggle(),
+    and some variables including pin, active_high, and initial_value.
+    documentation here: https://gpiozero.readthedocs.io/en/stable/api_output.html#outputdevice
+    '''
 
-    def _initialize_relay(self):
-        '''active_high = False initializes Sainsmart 4-channel relay board as off'''
-
-        return gpiozero.OutputDevice(self.pin, active_high=False)
+    def __init__(self, pin, *args, **kwargs):
+        '''
+        to use our own init in the Relay class, we must explicitly call
+        the base class's __init__, otherwise it will be overridden
+        more here: https://stackoverflow.com/questions/6396452/python-derived-class-and-base-class-attributes#6396839
+        '''
+        gpiozero.OutputDevice.__init__(self, pin, active_high=False, *args, **kwargs)
 
     def test_connection(self):
-        self.relay.toggle()
+        self.toggle()
         time.sleep(1)
-        self.relay.toggle()
+        self.toggle()
 
 
 if __name__ == '__main__':
