032e44e1471e1bec8f484e14b86ec57a35ab7c02
Unknown <brandon.aleson@gmail.com>
2017-12-09 19:12:40 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ node.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# murmur - class to represent a single node controlling 3 arms
+# 12/9/17
+# updated: 12/9/17
+
+from arm import Arm
+
+
+class Node(object):
+
+    def __init__(self):
+        pass
