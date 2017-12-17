8a6b0e7ed20b01b193438efc819571bb822b6937
Unknown <brandon.aleson@gmail.com>
2017-12-17 00:18:02 -0800

    figuring out ratios

--- arm.py
+++ arm.py
@@ -3,6 +3,7 @@
 # 12/9/17
 # updated: 12/13/17
 
+from datetime import timedelta
 from relay import Relay
 
 
@@ -16,9 +17,18 @@ class Arm(object):
         'middle': Relay(pins[1]),
         'upper': Relay([pins[2])
     }
+
+    total_time = total amount of time in seconds that an arm takes to go through its movement sequence
+    ratio = ratio expressing each actuator's piece of total_time
+
     '''
 
+    total_time = timedelta(seconds=180)
+
     actuators = ['lower', 'middle', 'upper']
+    ratio = [1, 3, 2]
+    ratio_total = sum(ratio)
+    seconds_split = [i/ratio_total * total_time.seconds for i in ratio]
 
     def __init__(self, pins, **kwargs):
         '''
@@ -28,6 +38,9 @@ class Arm(object):
 
         self.relays = self._initialize_relays(pins, **kwargs)
 
+    def _initialize_ratio(self):
+
+
     def _initialize_relays(self, pins, **kwargs):
         relays = [Relay(pin, **kwargs) for pin in pins]
 
@@ -36,3 +49,6 @@ class Arm(object):
     def test_connections(self):
         for actuator in self.actuators:
             self.relays[actuator].test_connection()
+
+    def run_forward(self):
+        pass
