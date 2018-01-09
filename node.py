a720c3744bbb3c6c9f4373a84645bbd59d231c81
Unknown <brandon.aleson@gmail.com>
2018-01-08 17:14:18 -0800

    add 4th pin to pin groupings

--- node.py
+++ node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # murmur - class to represent a single node controlling 3 arms
 # 12/9/17
-# updated: 12/16/17
+# updated: 1/8/17
 
 from arm import Arm
 
@@ -9,9 +9,9 @@ from arm import Arm
 class Node(object):
 
     pin_groupings = {
-        'left_arm': [17, 27, 22],
-        'middle_arm': [5, 6, 13],
-        'right_arm': [16, 20, 21]
+        'left_arm': [4, 17, 27, 22],
+        'middle_arm': [5, 6, 13, 19],
+        'right_arm': [12, 16, 20, 21]
     }
 
     def __init__(self, **kwargs):
