429bacd7b0ed428838d339cefc2cd69a106a7450
Unknown <brandon.aleson@gmail.com>
2018-01-18 14:57:36 -0800

    abstracted different movements into one function

--- control/timer.py
+++ control/timer.py
@@ -12,36 +12,51 @@ from datetime import datetime, timedelta
 1. start with M low; 2 second delay; repeat sequentially all the way to A
 2. 20 second delay after low movement
 3. start with A and fire both mid-ext and top; 5 second delay; repeat to L
+4. reverse starting with M top and mid-retract
 '''
 
 class Timer(object):
-    arms = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M']
+    arms_A_to_M = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M']
+    arms_M_to_A = arms_A_to_M[::-1]
 
     pauses = {
         'low': {
             'sequence': timedelta(seconds=2),
             'done': timedelta(seconds=10)
         },
-        'mid': {
+        'mid-ext_and_top': {
             'sequence': timedelta(seconds=2),
             'done': timedelta(seconds=10)
         }
     }
 
+    actions = {
+        'low': {
+            'order': arms_M_to_A,
+            'actuators': ['low'],
+            'activate': [True]
+        },
+        'mid-ext_and_top': {
+            'order': arms_A_to_M,
+            'actuators': ['mid-ext', 'top'],
+            'activate': [True, True]
+        }
+    }
 
-    # def __init__(self):
-    #     self.logger = self._initialize_logger()
-    #
-    # def _initialize_logger(self):
-    #     logger = logging.getLogger('timer')
-    #     logger.info('timer logger instantiated')
-    #
-    #     return logger
+
+    def __init__(self):
+        self.logger = self._initialize_logger()
+
+    def _initialize_logger(self):
+        logger = logging.getLogger('timer')
+        logger.info('timer logger instantiated')
+
+        return logger
 
     def _get_pause(self, pause):
         return datetime.now() + pause
 
-    def take_break(self, pause):
+    def _take_break(self, pause):
         pause = self._get_pause(pause)
 
         while True:
@@ -51,53 +66,35 @@ class Timer(object):
                 yield None
                 break
 
-
-    def fire_low(self):
-        order = self.arms[::-1]  # start with arm M
-        actuator = 'low'
-        activate = True
-
-        for arm in order:
-            pause = self._get_pause(self.pauses['low']['sequence'])
+    def _fire(self, action):
+        for arm in self.actions[action]['order']:
+            pause = self._get_pause(self.pauses[action]['sequence'])
 
             while True:
                 if datetime.now() <= pause:
                     yield None
                 else:
-                    yield (arm, actuator, True)
-                    break
-
-    def fire_mid_and_top(self):
-        order = self.arms
-        actuators = ['mid-ext', 'top']
-        activate = True
-
-        for arm in order:
-            pause = self._get_pause(self.pauses['mid']['sequence'])
-
-            while True:
-                if datetime.now() <= pause:
-                    yield None
-                else:
-                    yield (arm, actuators[0], True)
-                    yield (arm, actuators[1], True)
+                    actuators = self.actions[action]['actuators']
+                    activate = self.actions[action]['activate']
+                    for i in range(len(actuators)):
+                        yield (arm, actuators[i], activate[i])
                     break
 
     def wrapper(self):
         print('firing low')
-        for action in self.fire_low():
+        for action in self._fire('low'):
             yield action
 
         brk = self.pauses['low']['done']
         print('done firing low, taking a break for {} seconds...'.format(brk.seconds))
-        for pause in self.take_break(brk):
+        for pause in self._take_break(brk):
             yield pause
 
         print('firing mid and top')
-        for action in self.fire_mid_and_top():
+        for action in self._fire('mid-ext_and_top'):
             yield action
 
-        brk = self.pauses['mid']['done']
+        brk = self.pauses['mid-ext_and_top']['done']
         print('done firing mid-ext and top, taking a break for {} seconds...'.format(brk.seconds))
-        for pause in self.take_break(brk):
+        for pause in self._take_break(brk):
             yield pause
