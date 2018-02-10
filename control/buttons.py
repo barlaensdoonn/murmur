05bdc4b05b58ae2bd1ae83abfe5f356b227535a8
barlaensdoonn <brandon.aleson@gmail.com>
2018-02-10 15:51:13 -0800

    revert to constructing button in app

--- control/buttons.py
+++ control/buttons.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 # murmur - touch buttons
 # 1/27/18
-# updated: 2/9/18
+# updated: 2/10/18
 
-import kivy
 from kivy.app import App
 from kivy.uix.button import Button
 
 
-class Buttons(object):
-
-    def __init__(self):
-        self.button = self._make_button('buttons', self.callback)
+class ButtonsApp(App):
 
     def _make_button(self, text, callback):
         button = Button(text=text)
@@ -22,14 +18,11 @@ class Buttons(object):
     def callback(self, instance):
         print('button {} pressed'.format(instance.text))
 
-
-class MainApp(App):
-
     def build(self):
-        buttons = Buttons()
+        button = self._make_button('buttons', self.callback)
 
-        return buttons.button
+        return button
 
 
 if __name__ == '__main__':
-    MainApp().run()
+    ButtonsApp().run()
