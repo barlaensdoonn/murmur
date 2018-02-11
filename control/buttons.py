0179e11a946a852fa6f8875809fef1cabb2831c2
barlaensdoonn <brandon.aleson@gmail.com>
2018-02-11 11:00:47 -0800

    add colors

--- control/buttons.py
+++ control/buttons.py
@@ -5,13 +5,33 @@
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import Color, Rectangle
 
 
 class ButtonsApp(App):
 
-    def _make_button(self, text, callback):
-        button = Button(text=text)
-        button.bind(on_press=callback)
+    def set_properties(self):
+        '''hold button properties'''
+
+        self.properties = {
+            'start': {
+                'color': [0.2, 0.8, 0.2, 1],
+                'callback': self.callback
+            },
+            'pause': {
+                'color': [0.7, 0.7, 0.7, 1],
+                'callback': self.callback
+            },
+            'stop': {
+                'color': [0.88, 0.2, 0.2, 1],
+                'callback': self.callback
+            }
+        }
+
+    def _make_button(self, text):
+        button = Button(text=text.upper(), background_normal='', background_color=self.properties[text]['color'])
+        button.bind(on_press=self.properties[text]['callback'])
 
         return button
 
@@ -19,9 +39,25 @@ class ButtonsApp(App):
         print('button {} pressed'.format(instance.text))
 
     def build(self):
-        button = self._make_button('buttons', self.callback)
+        # Set up the layout:
+        layout = GridLayout(cols=3, spacing=30, padding=30, row_default_height=150)
 
-        return button
+        # Make the background gray:
+        with layout.canvas.before:
+            Color(.2, .2, .2, 1)
+            self.rect = Rectangle(size=(800, 600), pos=layout.pos)
+
+        # set button properties and make buttons
+        self.set_properties()
+        start_button = self._make_button('start')
+        pause_button = self._make_button('pause')
+        stop_button = self._make_button('stop')
+
+        layout.add_widget(start_button)
+        layout.add_widget(pause_button)
+        layout.add_widget(stop_button)
+
+        return layout
 
 
 if __name__ == '__main__':
