6a5aad9db905d86b8f31d8918bfd25fd46606676
barlaensdoonn <brandon.aleson@gmail.com>
2018-02-11 20:29:19 -0800

    refactor into 2 classes

--- control/buttons.py
+++ control/buttons.py
@@ -10,12 +10,19 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.graphics import Color, Rectangle
 
 
-class ButtonsApp(App):
+class ButtonsLayout(GridLayout):
 
-    def setup_buttons(self):
+    def __init__(self, **kwargs):
+        super(ButtonsLayout, self).__init__(**kwargs)
+        self.button_props = self._setup_buttons()
+        self.start_button = self._make_button('start')
+        self.pause_button = self._make_button('pause')
+        self.stop_button = self._make_button('stop')
+
+    def _setup_buttons(self):
         '''setup button properties'''
 
-        self.button_props = {
+        button_props = {
             'start': {
                 'type': Button,
                 'color': [0.2, 0.8, 0.2, 1],
@@ -33,6 +40,8 @@ class ButtonsApp(App):
             }
         }
 
+        return button_props
+
     def _make_button(self, text):
         button = self.button_props[text]['type'](text=text.upper(), background_normal='', background_color=self.button_props[text]['color'])
         button.bind(on_press=self.button_props[text]['callback'])
@@ -42,29 +51,26 @@ class ButtonsApp(App):
     def callback(self, instance):
         print('button {} pressed'.format(instance.text))
 
-    def change_text(self, event):
-        self.pause_button.text = 'RESUME' if event.text == 'PAUSE' else 'PAUSE'
+    def change_text(self, instance):
+        self.callback(instance)
+        self.pause_button.text = 'RESUME' if instance.text == 'PAUSE' else 'PAUSE'
+
+
+class ButtonsApp(App):
 
     def build(self):
-        # Set up the layout:
-        layout = GridLayout(cols=3, spacing=30, padding=30, row_default_height=150)
+        layout = ButtonsLayout(cols=3, spacing=30, padding=30, row_default_height=150)
 
         # Make the background gray:
         with layout.canvas.before:
             Color(.2, .2, .2, 1)
             self.rect = Rectangle(size=(800, 600), pos=layout.pos)
 
-        # set button properties and make buttons
-        self.setup_buttons()
-        self.start_button = self._make_button('start')
-        self.pause_button = self._make_button('pause')
-        self.stop_button = self._make_button('stop')
+            layout.add_widget(layout.start_button)
+            layout.add_widget(layout.pause_button)
+            layout.add_widget(layout.stop_button)
 
-        layout.add_widget(self.start_button)
-        layout.add_widget(self.pause_button)
-        layout.add_widget(self.stop_button)
-
-        return layout
+            return layout
 
 
 if __name__ == '__main__':
